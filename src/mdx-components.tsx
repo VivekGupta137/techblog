823cb0fbcc8bd0c41c42518de1a7637e852458c5
vivek gupta <vivekgpt107@gmail.com>
2024-10-27 09:55:07 +0530

    added enhancements for link

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,13 +2,12 @@ import type { MDXComponents } from "mdx/types";
 import Image, { ImageProps } from "next/image";
 import Link from "next/link";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table";
+import { GoArrowUpRight } from "react-icons/go";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 className="text-4xl text-primary font-gt-bold ">
-        {children}
-      </h1>
+      <h1 className="text-4xl text-primary font-gt-bold ">{children}</h1>
     ),
     h2: ({ children }) => (
       <h2 className="font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0 font-gt-bold">
@@ -29,21 +28,31 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {...(props as ImageProps)}
       />
     ),
-    
+
     p: ({ children }) => (
-      <p className="dark:text-muted-foreground mt-2 mb-4 font-gt-reg">{children}</p>
+      <p className="dark:text-muted-foreground mt-2 mb-4 font-gt-reg">
+        {children}
+      </p>
     ),
     a: ({ children, href }) => (
-      <Link
-        className="text-primary underline dark:text-muted-foreground dark:hover:bg-[#212121] hover:bg-gray-200 px-1 rounded-sm transition-all duration-300"
-        href={href as string}
-      >
-        {children}
-      </Link>
+        <Link
+          className=" inline-flex group text-primary underline dark:text-muted-foreground dark:hover:bg-[#212121] group-hover:bg-gray-200 px-1 rounded-sm transition-all duration-300"
+          href={href as string}
+        >
+          {children} <GoArrowUpRight className="inline-block size-4 text-blue-500 group-hover:text-green-500" />
+        </Link>
     ),
-    ul: ({ children }) => <ul className="list-disc ml-6 dark:text-muted-foreground font-gt-reg">{children}</ul>,
-    strong: ({ children }) => <strong className="font-gt-bold">{children}</strong>,
-    table: ({ children }) => <Table className="w-full rounded-md">{children}</Table>,
+    ul: ({ children }) => (
+      <ul className="list-disc ml-6 dark:text-muted-foreground font-gt-reg">
+        {children}
+      </ul>
+    ),
+    strong: ({ children }) => (
+      <strong className="font-gt-bold">{children}</strong>
+    ),
+    table: ({ children }) => (
+      <Table className="w-full rounded-md">{children}</Table>
+    ),
     thead: ({ children }) => <TableHeader>{children}</TableHeader>,
     tbody: ({ children }) => <TableBody>{children}</TableBody>,
     th: ({ children }) => <TableHead>{children}</TableHead>,
